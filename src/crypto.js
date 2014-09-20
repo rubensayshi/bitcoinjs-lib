534320a3168146e2f77d5f1e78d9c0b7ae5660e1
Daniel Cousens <github@dcousens.com>
2014-09-20 11:20:55 +1000

    crypto: remove Hmac* exposure in API

--- src/crypto.js
+++ src/crypto.js
@@ -1,13 +1,5 @@
 var crypto = require('crypto')
 
-function hash160(buffer) {
-  return ripemd160(sha256(buffer))
-}
-
-function hash256(buffer) {
-  return sha256(sha256(buffer))
-}
-
 function ripemd160(buffer) {
   return crypto.createHash('rmd160').update(buffer).digest()
 }
@@ -20,15 +12,12 @@ function sha256(buffer) {
   return crypto.createHash('sha256').update(buffer).digest()
 }
 
-// FIXME: Name not consistent with others
-function HmacSHA256(buffer, secret) {
-  console.warn('Hmac* functions are deprecated for removal in 2.0.0, use node crypto instead')
-  return crypto.createHmac('sha256', secret).update(buffer).digest()
+function hash160(buffer) {
+  return ripemd160(sha256(buffer))
 }
 
-function HmacSHA512(buffer, secret) {
-  console.warn('Hmac* functions are deprecated for removal in 2.0.0, use node crypto instead')
-  return crypto.createHmac('sha512', secret).update(buffer).digest()
+function hash256(buffer) {
+  return sha256(sha256(buffer))
 }
 
 module.exports = {
@@ -36,7 +25,5 @@ module.exports = {
   sha1: sha1,
   sha256: sha256,
   hash160: hash160,
-  hash256: hash256,
-  HmacSHA256: HmacSHA256,
-  HmacSHA512: HmacSHA512
+  hash256: hash256
 }
